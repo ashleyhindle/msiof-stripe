a94d3e28d01c7f4e295304f85cb1acd90b8412fc
Ashley Hindle <ashley@smellynose.com>
2014-11-21 21:02:58 +0000

    create stripe customer on register and store customerid

--- src/Tah/MsiofStripe/MsiofStripeServiceProvider.php
+++ src/Tah/MsiofStripe/MsiofStripeServiceProvider.php
@@ -6,6 +6,9 @@ use Silex\ServiceProviderInterface;
 use Silex\ControllerProviderInterface;
 use Silex\ControllerCollection;
 use Silex\ServiceControllerResolver;
+use Symfony\Component\HttpFoundation\Request;
+use SimpleUser\UserEvents;
+use SimpleUser\UserEvent;
 
 /**
  * Class: MsiofStripeServiceProvider
@@ -15,14 +18,42 @@ use Silex\ServiceControllerResolver;
  */
 class MsiofStripeServiceProvider implements ServiceProviderInterface, ControllerProviderInterface
 {
+		  /**
+		  * register
+		  *
+		  * @param Application $app
+		  */
 		  public function register(Application $app)
 		  {
-
+					 $app['msiof.stripe'] = [
+								'keys' => []
+					 ];
 		  }
 
+		  /**
+		  * boot
+		  *
+		  * @param Application $app
+		  */
 		  public function boot(Application $app)
 		  {
+					 if (empty($app['msiof.stripe']['keys']['secret'])) {
+								throw new \RuntimeException('Stripe key not set msiof.stripe[keys][secret].  Cannot continue.');
+					 }
 
+					 if (empty($app['msiof.stripe']['keys']['publishable'])) {
+								throw new \RuntimeException('Stripe key not set msiof.stripe[keys][publishable].  Cannot continue.');
+					 }
+					 \Stripe::setApiKey($app['msiof.stripe']['keys']['secret']);
+
+					 $app['dispatcher']->addListener(UserEvents::AFTER_INSERT, function(UserEvent $event) use ($app) {
+								$user = $event->getUser();
+								$customer = \Stripe_Customer::create([
+										  'email' => $user->getEmail(),
+								]);
+								$user->setCustomField('stripe_customer_id', $customer->id);
+								$app['user.manager']->update($user);
+					 });
 		  }
 
 		  /**
@@ -39,7 +70,62 @@ class MsiofStripeServiceProvider implements ServiceProviderInterface, Controller
 
 					 $controllers->get('/', function (Application $app) {
 								return 'It works';
+					 })->bind('msiof-stripe-index');
+
+					 $controllers->get('/pay', function (Application $app) {
+								if (!$app['user']) {
+										  return $app->redirect('user.login');
+								}
+
+								$form = '
+								<form action="" method="POST">
+								<script
+								src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+								data-key="pk_test_2bpghGfYvZb4cS2rYIhpcC31"
+								data-amount="2000"
+								data-name="Unlimited MSIOF"
+								data-description="Unlimited (&pound; 20/month)"
+								data-image="/128x128.png"
+								data-currency="GBP"
+								data-allowrememberme="false"
+								data-email="ashley@smellynose.com"
+								>
+								</script>
+								</form>
+								';
+
+								return $form;
+					 })->bind('msiof-stripe-pay-form');
+
+					 $controllers->post('/pay', function(Application $app, Request $request) {
+								if (!$app['user']) {
+										  return $app->redirect('user.login');
+								}
+
+								\Stripe::setApiKey('sk_test_26P4i0fynum9NZWXomS2wlTd');
+								$customer = \Stripe_Customer::create([
+										  'email' => $request->get('stripeEmail'),
+										  'card'  => $request->get('stripeToken')
+								]);
+
+								$app['user']->setCustomField('stripe_customer_id', $customer->id);
+								$app['user.manager']->update($app['user']);
+
+								$charge = \Stripe_Charge::create([
+										  'customer' => $customer->id,
+										  'amount'   => 2000,
+										  'currency' => 'gbp'
+								]);
+
+								var_dump($charge);
+
+								return print_r($_POST, true);
+					 })->bind('msiof-stripe-pay-post');
+
+					 $controllers->post('/webhook', function(Application $app, Request $request) {
+								return 'Thanks';
 					 });
+
 
 					 return $controllers;
 		  }
